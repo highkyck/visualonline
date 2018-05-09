767ef24d1384cabe42030f3a237a297bbe7a1450
liubang <it.liubang@gmail.com>
2018-05-09 16:29:28 +0800

    编写简单的ws服务

--- conf/config.php
+++ conf/config.php
@@ -3,4 +3,8 @@
 return [
     'app_directory' => APP_PATH . 'app',
     'log_path'      => '/data/logs/scripts/',
+    'ws_server' => [
+        'host'  => '192.168.8.11',
+        'port'  => '9501',
+    ]
 ];
